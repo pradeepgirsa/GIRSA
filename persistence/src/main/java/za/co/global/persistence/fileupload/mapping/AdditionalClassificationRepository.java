79bdd50ecce29ef38aadf6d967ef417be052147d
Sharanya Reddy <sharanyareddy@perago.com>
2018-11-08 21:29:22 +0200

    daily pricing controller changes.

--- persistence/src/main/java/za/co/global/persistence/fileupload/mapping/AdditionalClassificationRepository.java
+++ persistence/src/main/java/za/co/global/persistence/fileupload/mapping/AdditionalClassificationRepository.java
@@ -4,9 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.co.global.domain.fileupload.mapping.AdditionalClassification;
 
+import java.util.List;
+
 @Repository
 public interface AdditionalClassificationRepository  extends JpaRepository<AdditionalClassification, Long> {
 
     AdditionalClassification findByAlphaCode(String alphaCode);
+
+    List<AdditionalClassification> findByIndustryAndSectorAndSuperSectorAndSubSector(String industry,
+                                                                                     String sector, String superSector,
+                                                                                     String subSector);
 
 }
